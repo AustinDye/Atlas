0a662765b4ce51c27cf7efa1ae617eb7072a1c95
Austin Dye <FireAtWill012496>
2022-08-30 14:03:22 -0600

    Company Portfolio

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template></template>
+
+<script>
+import { computed } from "vue";
+import { AppState } from "./AppState";
+
+export default {
+  name: "App",
+  setup() {
+    return {
+      appState: computed(() => AppState),
+    };
+  },
+};
+</script>
+<style lang="scss">
+@import "./assets/scss/main.scss";
+</style>
